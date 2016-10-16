531752157b77e29c9857a5ebb6c50d8b1f4f608a
Michael Blake <Michael Blake>
2016-10-16 19:28:45 +1000

    added flexboxgrid

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,10 +1,30 @@
 import React from "react";
 import AppBar from "material-ui/AppBar";
+import Drawer from 'material-ui/Drawer';
 
-export const Header = () => {
-    return (
-        <AppBar>
-            <AppBar title="Boilerplate" />
-        </AppBar>
-    )
+export class Header extends React.Component<any, any> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false
+        };
+    }
+
+    handleToggle = () => {
+        this.setState({
+            open: !this.state.open
+        });
+    }
+
+
+    render() {
+        return (
+            <AppBar onLeftIconButtonTouchTap={this.handleToggle}>
+                <Drawer open={this.state.open}>
+                    <AppBar title="Boilerplate" showMenuIconButton={false} />
+                    
+                </Drawer>
+            </AppBar>
+        )
+    }
 }
--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -5,6 +5,10 @@ export const HomePage = () => {
     return (
         <div>Home page<br />
             <Link to="/about">Protected Content</Link>
+            <div className="row">
+                <div className="col-sm-8">a</div>
+                <div className="col-sm-4">b</div>
+            </div>
         </div>
     );
 };
